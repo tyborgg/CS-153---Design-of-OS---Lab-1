d69974c5f5b8b060835cb8f9acb5260e9b483f55
Tyler Pastor <tpast001@sledge.cs.ucr.edu>
2022-03-10 21:04:23 -0800

    Lab 3 code

--- shm.c
+++ shm.c
@@ -8,42 +8,70 @@
 #include "spinlock.h"
 
 struct {
-  struct spinlock lock;
-  struct shm_page {
-    uint id;
-    char *frame;
-    int refcnt;
-  } shm_pages[64];
+    struct spinlock lock;
+    struct shm_page {
+        uint id;
+        char *frame;
+        int refcnt;
+    } shm_pages[64];
 } shm_table;
 
 void shminit() {
-  int i;
-  initlock(&(shm_table.lock), "SHM lock");
-  acquire(&(shm_table.lock));
-  for (i = 0; i< 64; i++) {
-    shm_table.shm_pages[i].id =0;
-    shm_table.shm_pages[i].frame =0;
-    shm_table.shm_pages[i].refcnt =0;
-  }
-  release(&(shm_table.lock));
+    int i;
+    initlock(&(shm_table.lock), "SHM lock");
+    acquire(&(shm_table.lock));
+    for (i = 0; i< 64; i++) {
+        shm_table.shm_pages[i].id =0;
+        shm_table.shm_pages[i].frame =0;
+        shm_table.shm_pages[i].refcnt =0;
+    }
+    release(&(shm_table.lock));
 }
 
 int shm_open(int id, char **pointer) {
+    struct proc *curproc = myproc();
+    acquire(&(shm_table.lock));
+    int i;
 
-//you write this
+    for (i = 0; i< 64; i++) {
+        if(id == shm_table.shm_pages[i].id){
+            mappages(curproc->pgdir, (void *)curproc->sz, PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W|PTE_U);
+            shm_table.shm_pages[i].refcnt++;
+        }
+        else if(shm_table.shm_pages[i].id == 0){
+            shm_table.shm_pages[i].id = id;
+            shm_table.shm_pages[i].frame = kalloc();
+            shm_table.shm_pages[i].refcnt++;
+            memset(shm_table.shm_pages[i].frame, 0, PGSIZE);
+            mappages(curproc->pgdir, (void *)curproc->sz, PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W|PTE_U);
+        }
 
+        *pointer = (char *)curproc->sz;
+        curproc->sz += PGSIZE;
+    }
 
+    release(&(shm_table.lock));
 
-
-return 0; //added to remove compiler warning -- you should decide what to return
+    return (int)*pointer; //added to remove compiler warning -- you should decide what to return
 }
 
 
 int shm_close(int id) {
-//you write this too!
+    acquire(&(shm_table.lock));
+    int i;
 
+    for (i = 0; i< 64; i++) {
+        if(id == shm_table.shm_pages[i].id){
+            shm_table.shm_pages[i].refcnt--;
+        }
+        if(shm_table.shm_pages[i].refcnt == 0){
+            shm_table.shm_pages[i].id = 0;
+            shm_table.shm_pages[i].frame = 0;
+            shm_table.shm_pages[i].refcnt = 0;
+        }
+    }
 
+    release(&(shm_table.lock));
 
-
-return 0; //added to remove compiler warning -- you should decide what to return
+    return 0; //added to remove compiler warning -- you should decide what to return
 }
